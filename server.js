0e9bf9e9585c560d344a41c98ac335ded6f3f461
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-09-01 00:32:30 +0600

    add server side code

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+
+// configure env
+dotenv.config();
+
+// database config
+connectDB();
+
+// reset object
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+// routes
+app.use("/api/v1/auth", authRoutes);
+
+// rest api
+app.get("/", (req, res) => {
+  res.send("<h1><em>Welcome to e-commerce app</em></h1>");
+});
+
+// PORT
+const PORT = process.env.PORT || 8080;
+
+// run listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgYellow
+      .black
+  );
+});
