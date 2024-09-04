5b88c73ceb7257975312a6521cb6e1c40dd0d882
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-09-05 00:40:17 +0600

    create and add routing in login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Contact from "./pages/Contact";
 import Policy from "./pages/Policy";
 import PageNotFound from "./pages/PageNotFound";
 import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
 import "./App.css";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/policy" element={<Policy />} />
---
+++ client/src/pages/Auth/Login.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Layout from "../../components/Layout/Layout";
+
+const Login = () => {
+  return (
+    <Layout>
+      <div className="min-h-[50vh] flex justify-center items-center">
+        <h2 className="text-3xl font-bold md:text-5xl mb-4 md:mb-8 lg:mb-12">
+          Login Page
+        </h2>
+      </div>
+    </Layout>
+  );
+};
+
+export default Login;
