92c8d954d915d8c432140421110b863a999ee09f
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-10-03 23:51:47 +0600

    Created admin menu and dynamically loaded data in Admin Dashboard

---
+++ client/src/components/Layout/AdminMenu.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const AdminMenu = () => {
+  return (
+    <>
+      <div className="flex flex-col gap-4 items-center">
+        <h3 className="text-3xl">Admin Panel</h3>
+        <NavLink to="/dashboard/admin/create-category">Create Category</NavLink>
+        <NavLink to="/dashboard/admin/create-product">Create Product</NavLink>
+        <NavLink to="/dashboard/admin/users">Users</NavLink>
+      </div>
+    </>
+  );
+};
+
+export default AdminMenu;
--- client/src/pages/Admin/AdminDashboard.jsx
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Layout from "./../../components/Layout/Layout";
+import AdminMenu from "../../components/Layout/AdminMenu";
+import { useAuth } from "../../context/auth";
 
 const AdminDashboard = () => {
+  const [auth, setAuth] = useAuth();
   return (
     <Layout>
-      <h1>Admin Dashboard</h1>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-3">
+          <div className="border border-red-500 col-span-1 p-5">
+            <AdminMenu />
+          </div>
+          <div className="border border-teal-500 col-span-2 p-5">
+            <h3>Admin Name: {auth?.user?.name}</h3>
+            <h3>Admin Email: {auth?.user?.email}</h3>
+            <h3>Admin Address: {auth?.user?.address}</h3>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
