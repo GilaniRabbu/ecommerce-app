29441f98d3f7f023f6f37bc9866fefd4c684642e
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-10-04 00:11:57 +0600

    Updated layout and titles in Users, CreateCategory, and CreateProduct

--- client/src/pages/Admin/CreateProduct.jsx
+++ client/src/pages/Admin/CreateProduct.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import AdminMenu from "../../components/Layout/AdminMenu";
 
 const CreateProduct = () => {
   return (
-    <Layout>
-      <h1>Create Product</h1>
+    <Layout title={"Dashboard - Create Product"}>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-3">
+          <div className="border border-red-500 col-span-1 p-5">
+            <AdminMenu />
+          </div>
+          <div className="border border-teal-500 col-span-2 p-5">
+            <h1>Create Product</h1>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
--- client/src/pages/Admin/Users.jsx
+++ client/src/pages/Admin/Users.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import AdminMenu from "../../components/Layout/AdminMenu";
 
 const Users = () => {
   return (
-    <Layout>
-      <h1>All Users</h1>
+    <Layout title={"Dashboard - All Users"}>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-3">
+          <div className="border border-red-500 col-span-1 p-5">
+            <AdminMenu />
+          </div>
+          <div className="border border-teal-500 col-span-2 p-5">
+            <h1>All Users</h1>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
