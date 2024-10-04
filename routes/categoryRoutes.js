ba821bf2f45c3abdaa6275336c7b044bfd804325
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-10-04 16:49:14 +0600

    Added functionality to routing for CreateCategory pages

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { isAdmin, requireSignIn } from './../middleware/authMiddleware';
+import { isAdmin, requireSignIn } from "./../middleware/authMiddleware.js";
+import { createCategoryController } from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 // Routes
-router.post("create-category", requireSignIn, isAdmin, createCategoryController);
+router.post("/create-category", requireSignIn, isAdmin, createCategoryController);
 
 export default router;
