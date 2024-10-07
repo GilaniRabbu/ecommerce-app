89cc8e13e72172a3a16b00e3c480d09c42049c68
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-10-07 16:53:55 +0600

    Implemented Products API CRUD with create, read, update, and delete operations using Node.js REST API

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,46 @@
 import express from "express";
+import formidable from "express-formidable";
 import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
+import {
+    createProductController,
+    deleteProductController,
+    getProductController,
+    getSingleProductController,
+    productPhotoController,
+    updateProductController,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
 // Routes
-router.post("/create-product", requireSignIn, isAdmin, createProductController);
+// Create Product
+router.post(
+    "/create-product",
+    requireSignIn,
+    isAdmin,
+    formidable(),
+    createProductController,
+);
+
+// Update Product
+router.put(
+    "/update-product/:pid",
+    requireSignIn,
+    isAdmin,
+    formidable(),
+    updateProductController,
+);
+
+// Get All Products
+router.get("/get-product", getProductController);
+
+// Get Single Product
+router.get("/get-product/:slug", getSingleProductController);
+
+// Get Photo
+router.get("/product-photo/:pid", productPhotoController);
+
+// Delete Product
+router.delete("/product/:pid", deleteProductController);
 
 export default router;
