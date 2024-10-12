858b6bf233f0c45745e3670659e7000a6228d756
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-10-12 18:38:03 +0600

    Added category edit and delete features to CreateCategory pages

--- client/src/components/Form/CategoryForm.jsx
+++ client/src/components/Form/CategoryForm.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const CategoryForm = ({ handleSubmit, value, setValue }) => {
   return (
     <>
-      <div className="mx-auto mb-4 max-w-sm pb-4">
+      <div className="max-w-md my-3 py-3">
         <form onSubmit={handleSubmit}>
           <input
             type="text"
-            className="mb-4 block h-9 w-full rounded-md border border-solid border-black px-4 py-2 pl-14 text-sm text-black placeholder:text-black"
+            className="block h-9 w-full rounded-md px-4 py-2 pl-5 text-sm border border-solid border-black text-black placeholder:text-black"
             placeholder="Enter New Category"
             value={value}
             onChange={(e) => setValue(e.target.value)}
@@ -16,7 +16,7 @@ const CategoryForm = ({ handleSubmit, value, setValue }) => {
           <input
             type="submit"
             value="Submit"
-            className="mt-4 inline-block w-full cursor-pointer items-center rounded-md bg-black px-4 py-2 text-center font-semibold text-white"
+            className="mt-4 inline-block w-full cursor-pointer items-center rounded-md px-4 py-2 text-center font-semibold bg-black text-white"
           />
         </form>
       </div>
--- client/src/pages/Admin/CreateCategory.jsx
+++ client/src/pages/Admin/CreateCategory.jsx
@@ -4,10 +4,14 @@ import axios from "axios";
 import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import CategoryForm from "../../components/Form/CategoryForm";
+import { Modal } from "antd";
 
 const CreateCategory = () => {
   const [categories, setCategories] = useState([]);
   const [name, setName] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const [updatedName, setUpdatedName] = useState("");
 
   // Handle Form
   const handleSubmit = async (e) => {
@@ -17,7 +21,8 @@ const CreateCategory = () => {
         name,
       });
       if (data?.success) {
-        toast.success(`${name} is created`);
+        toast.success(`${name} is Created`);
+        getAllCategory();
       } else {
         toast.error(data.message);
       }
@@ -27,7 +32,7 @@ const CreateCategory = () => {
     }
   };
 
-  // Get all categories
+  // Get all category
   const getAllCategory = async () => {
     try {
       const { data } = await axios.get("/api/v1/category/get-category");
@@ -44,16 +49,57 @@ const CreateCategory = () => {
     getAllCategory();
   }, []);
 
+  // Update Category
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        `/api/v1/category/update-category/${selected._id}`,
+        { name: updatedName }
+      );
+      if (data.success) {
+        toast.success(`${updatedName} is Updated`);
+        setSelected(null);
+        setUpdatedName("");
+        setVisible(false);
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something Went Wrong");
+    }
+  };
+
+  // Delete Category
+  const handleDelete = async (pid) => {
+    try {
+      const { data } = await axios.delete(
+        `/api/v1/category/delete-category/${pid}`
+      );
+      if (data.success) {
+        toast.success("Category is Deleted");
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something Went Wrong");
+    }
+  };
+
   return (
     <Layout title={"Dashboard - Create Category"}>
       <div className="container mx-auto">
         <div className="grid grid-cols-3 gap-3">
-          <div className="border border-red-500 col-span-1 p-5">
+          <div className="border border-black col-span-1 p-5">
             <AdminMenu />
           </div>
-          <div className="border border-teal-500 col-span-2 p-5">
-            <h1>Manage Category</h1>
-            <div className="p-3">
+          <div className="border border-black col-span-2 p-5">
+            <h1 className="text-3xl">Manage Category</h1>
+            <div>
               <CategoryForm
                 handleSubmit={handleSubmit}
                 value={name}
@@ -71,12 +117,29 @@ const CreateCategory = () => {
                 <tbody>
                   {categories?.map((c) => (
                     <>
-                      <tr>
+                      <tr key={c._id}>
                         <td className="p-2 border border-slate-300" key={c._id}>
                           {c.name}
                         </td>
                         <td className="p-2 border border-slate-300">
-                          <button className="">Edit</button>
+                          <button
+                            className="mr-2 text-blue-600"
+                            onClick={() => {
+                              setVisible(true);
+                              setUpdatedName(c.name);
+                              setSelected(c);
+                            }}
+                          >
+                            Edit
+                          </button>
+                          <button
+                            className="text-red-600"
+                            onClick={() => {
+                              handleDelete(c._id);
+                            }}
+                          >
+                            Delete
+                          </button>
                         </td>
                       </tr>
                     </>
@@ -84,6 +147,17 @@ const CreateCategory = () => {
                 </tbody>
               </table>
             </div>
+            <Modal
+              onCancel={() => setVisible(false)}
+              footer={null}
+              open={visible}
+            >
+              <CategoryForm
+                value={updatedName}
+                setValue={setUpdatedName}
+                handleSubmit={handleUpdate}
+              />
+            </Modal>
           </div>
         </div>
       </div>
