bac1bd47557de22dcf117ceaa2c01d0de3c496aa
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-10-13 23:38:14 +0600

    Added functionality to fetch product details

--- client/src/pages/Admin/Products.jsx
+++ client/src/pages/Admin/Products.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  // Get all products
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/product/get-product");
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+      toast.error("Something Went Wrong");
+    }
+  };
+
+  // Lifecycle Method
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
     <Layout title={"Dashboard - All Products"}>
       <div className="container mx-auto">
@@ -12,6 +33,21 @@ const Products = () => {
           </div>
           <div className="border border-teal-500 col-span-2 p-5">
             <h1>All Products List</h1>
+            <div className="mx-auto grid max-w-5xl justify-items-center gap-5 sm:grid-cols-2 md:grid-cols-3 md:gap-4 lg:gap-6">
+              {products?.map((p) => (
+                <Link key={p._id} to={`/dashboard/admin/product/${p.slug}`}>
+                  <div className="flex max-w-sm flex-col items-center gap-4 rounded-md border border-solid border-gray-300 px-8 py-6 md:max-w-full md:items-start">
+                    <img
+                      src={`/api/v1/product/product-photo/${p._id}`}
+                      alt={p.name}
+                      className="mb-4 mx-auto inline-block w-40 object-cover"
+                    />
+                    <p className="font-bold">{p.name}</p>
+                    <p className="text-sm text-gray-500">{p.description}</p>
+                  </div>
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       </div>
