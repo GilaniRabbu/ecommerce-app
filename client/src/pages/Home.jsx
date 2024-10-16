7f1d8deb65cb3614dece3482afb6edcb4f04502e
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-10-16 22:35:20 +0600

    Added functionality to display products on homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Checkbox } from "antd";
 import Layout from "../components/Layout/Layout";
-import { useAuth } from "../context/auth";
 
 const Home = () => {
-  const [auth, setAuth] = useAuth();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [checked, setChecked] = useState([]);
+
+  // Get all category
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
+  // Get Products
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/product/get-product");
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Filter by Category
+  const handleFilter = async (value, id) => {
+    let all = [...checked];
+    if (value) {
+      all.push(id);
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+  };
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
-    <Layout title={"Best Offers"}>
-      <div className="min-h-[50vh] p-20">
-        <h1 className="text-4xl">Home Page</h1>
-        <pre>{JSON.stringify(auth, null, 4)}</pre>
+    <Layout title={"All Products - Best Offers"}>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-3">
+          <div className="border border-black col-span-1 p-5">
+            <h4>Filter By Category</h4>
+            <div className="flex flex-col">
+              {categories?.map((c) => (
+                <Checkbox
+                  key={c._id}
+                  onChange={(e) => handleFilter(e.target.checked, c._id)}
+                >
+                  {c.name}
+                </Checkbox>
+              ))}
+            </div>
+          </div>
+          <div className="border border-black col-span-2 p-5">
+            {JSON.stringify(checked, null, 4)}
+            <h1>All Products</h1>
+            <div className="flex flex-wrap">
+              {products?.map((p) => (
+                <div className="flex max-w-sm flex-col items-center gap-4 rounded-md border border-solid border-gray-300 px-8 py-6 md:max-w-full md:items-start">
+                  <img
+                    src={`/api/v1/product/product-photo/${p._id}`}
+                    alt={p.name}
+                    className="mb-4 mx-auto inline-block w-40 object-cover"
+                  />
+                  <p className="font-bold">{p.name}</p>
+                  <p className="text-sm text-gray-500">{p.description}</p>
+                  <button>More Details</button>
+                  <button>Add to Cart</button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );
