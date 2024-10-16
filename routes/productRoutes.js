391490565e5ed48cec19b76c171e2731c11e1162
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-10-16 23:32:32 +0600

    Created controllers and routes for product filtering

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,12 +2,13 @@ import express from "express";
 import formidable from "express-formidable";
 import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
 import {
-    createProductController,
-    deleteProductController,
-    getProductController,
-    getSingleProductController,
-    productPhotoController,
-    updateProductController,
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productFiltersController,
+  productPhotoController,
+  updateProductController,
 } from "../controllers/productController.js";
 
 const router = express.Router();
@@ -15,20 +16,20 @@ const router = express.Router();
 // Routes
 // Create Product
 router.post(
-    "/create-product",
-    requireSignIn,
-    isAdmin,
-    formidable(),
-    createProductController,
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController,
 );
 
 // Update Product
 router.put(
-    "/update-product/:pid",
-    requireSignIn,
-    isAdmin,
-    formidable(),
-    updateProductController,
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController,
 );
 
 // Get All Products
@@ -42,5 +43,8 @@ router.get("/product-photo/:pid", productPhotoController);
 
 // Delete Product
 router.delete("/product/:pid", deleteProductController);
+
+// Filter Product
+router.get("/product-filters", productFiltersController);
 
 export default router;
