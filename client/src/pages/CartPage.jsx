89979f143017ce8d986f007e7f9ef110370c79c4
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-10-19 22:11:23 +0600

    Created cart page and added routing

---
+++ client/src/pages/CartPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const CartPage = () => {
+  return (
+    <Layout>
+      <div className="container mx-auto">
+        <h1>My Cart</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default CartPage;
