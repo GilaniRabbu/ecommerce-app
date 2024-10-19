3adddebb262063ba03850167f960b155f114a3c0
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-10-19 17:08:33 +0600

    Created CategoryProduct page and added routing

---
+++ client/src/pages/CategoryProduct.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const CategoryProduct = () => {
+  return (
+    <Layout>
+      <div className="container">
+        <div className="flex">
+          <h1>Category Product</h1>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default CategoryProduct;
