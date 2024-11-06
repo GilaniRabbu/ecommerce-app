b45ad78dc98ff63a754e62b06032418b587cdca6
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-11-07 01:05:58 +0600

    update server side

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Resolving dirname for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // configure env
 dotenv.config();
@@ -22,17 +27,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-// app.use(express.static(path.join(__dirname, "./client/dist")));
+
+// Use the Client app
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-// rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
-});
+// Render Client
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/client/dist/index.html")));
 
 // PORT
 const PORT = process.env.PORT || 8080;
