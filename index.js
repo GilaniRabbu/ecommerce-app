648b29bf07873dec07c258c08cc4a37c2f9ecae8
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-11-22 01:32:12 +0600

    update code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
@@ -7,12 +6,6 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Resolving dirname for ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // configure env
 dotenv.config();
@@ -28,24 +21,19 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-// Use the Client app
-app.use(express.static(path.join(__dirname, "/client/dist")));
-
 // routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-// Render Client
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/client/dist/index.html")));
+app.get("/", (req, res) => {
+  res.send("<p>E-Commerce Web App</p>");
+});
 
 // PORT
 const PORT = process.env.PORT || 8080;
 
 // run listen
 app.listen(PORT, () => {
-  console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgYellow
-      .black
-  );
+  console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`);
 });
